64917a36bfab2fa39ef4c8db21dc1c9bfc43e413
Konstantin Astakhov <kastakhov@usetech.com>
2024-05-28 22:24:41 +0700

    relay-node-only workflow test-18

--- tests/scripts/wait_for_first_block.sh
+++ tests/scripts/wait_for_first_block.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "$0")
-RPC_URL="http://127.0.0.1:9833"
 
 . "$DIR/functions.sh"
 
 last_block_id=0
 block_id=0
 function get_block {
-	block_id_hex=$(do_rpc chain_getHeader | jq -r .result.number)
-	block_id=$((block_id_hex))
-	echo Id = $block_id
+    block_id_hex=$(do_rpc chain_getHeader | jq -r .result.number)
+    block_id=$((block_id_hex))
+    echo Id = $block_id
 }
 
 function had_new_block {
-	last_block_id=$block_id
-	get_block
-	if (( last_block_id != 0 && block_id > last_block_id )); then
-		return 0
-	fi
-	return 1
+    last_block_id=$block_id
+    get_block
+    if (( last_block_id != 0 && block_id > last_block_id )); then
+        return 0
+    fi
+    return 1
 }
 
 function reset_check {
-	last_block_id=0
-	block_id=0
+    last_block_id=0
+    block_id=0
 }
 
 while ! had_new_block; do
-	echo "Waiting for next block..."
-	sleep 12
+    echo "Waiting for next block..."
+    sleep 12
 done
 reset_check
 
@@ -37,8 +36,8 @@ echo "Chain is running, but lets wait for another block after a minute, to avoid
 sleep 60
 
 while ! had_new_block; do
-	echo "Waiting for another block..."
-	sleep 12
+    echo "Waiting for another block..."
+    sleep 12
 done
 
 echo "Chain is running!"
