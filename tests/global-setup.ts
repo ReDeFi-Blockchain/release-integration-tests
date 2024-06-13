fd564b52e2eb964faa77c40be62451748dbd9355
Max Andreev <maxick20@gmail.com>
2024-06-13 08:08:45 +0000

    fix(tests): todos

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -65,23 +65,22 @@ export async function globalSetup() {
 
     console.log(">>> The balances have been topped up!");
 
-    // TODO: uncomment with XCM
-    // if (testConfig.isCrosschain && chainId === NETWORK_CONSTANTS.L2.CHAIN_ID) {
-    //   console.log(">>> Configuring XCM...");
+    if (testConfig.isCrosschain && chainId === NETWORK_CONSTANTS.L2.CHAIN_ID) {
+      console.log(">>> Configuring XCM...");
 
-    //   const setXcmV3tx = sub.api.tx.sudo.sudo(
-    //     sub.api.tx.polkadotXcm.forceXcmVersion(
-    //       {
-    //         parents: 1,
-    //         interrior: "Here",
-    //       },
-    //       3,
-    //     ),
-    //   );
+      const setXcmV3tx = sub.api.tx.sudo.sudo(
+        sub.api.tx.polkadotXcm.forceXcmVersion(
+          {
+            parents: 1,
+            interrior: "Here",
+          },
+          3,
+        ),
+      );
 
-    //   await sub.utils.signAndSend(sub.donor, setXcmV3tx);
-    //   console.log(">>> XCM V3 configured");
-    // }
+      await sub.utils.signAndSend(sub.donor, setXcmV3tx);
+      console.log(">>> XCM V3 configured");
+    }
   }
 
   process.exit(0);
